0bdd5d58d68be4e09065b4015ec58cdb37f2bdad
MF80 <mike.fenton@gmail.com>
2016-03-01 13:19:10 -0500

    Updated project
    Changed name

---
+++ MudBot/MudBot/Locations/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MudBot.Locations {
+    class Room {
+        int MapID;
+        int RoomID;
+        string Name;
+        int ShopID;
+        int NpcID;
+        int CommandID;
+        string Lair;
+        string Placed;
+
+        private enum Commands {
+            goPortal,
+            goPath
+        };
+    }
+}
--- MudBot/MudBot/Properties/AssemblyInfo.cs
+++ MudBot/MudBot/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("C-Sharp Usage Example")]
+[assembly: AssemblyTitle("MudBot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Walbury Software")]
-[assembly: AssemblyProduct("C-Sharp Usage Example")]
-[assembly: AssemblyCopyright("Copyright © Walbury Software 2007")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MudBut")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
